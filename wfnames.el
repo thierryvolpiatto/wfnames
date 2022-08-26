8028da52984554ee08c05f5439186c8ddbca48a6
Thierry Volpiatto <thievol@posteo.net>
2022-08-26 21:43:02 +0200

    Fix headers

--- wfnames.el
+++ wfnames.el
@@ -1,7 +1,27 @@
-;;; wfnames.el -- Edit marked files. -*- lexical-binding:t -*-
-;;
+;;; wfnames.el --- Edit marked files. -*- lexical-binding:t -*-
 
-;;; Description:
+;; Author: Thierry Volpiatto <thierry.volpiatto@gmail.com>
+;; Copyright (C) 2022 Thierry Volpiatto, all rights reserved.
+;; X-URL: https://github.com/thierryvolpiatto/wfnames
+
+;; Compatibility: GNU Emacs 24.1+
+;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
 ;; A mode to edit filenames, similar to wdired.
 
 ;; TODO:
