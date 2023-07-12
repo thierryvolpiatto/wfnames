4447417c58b063df1fca1ba4bec9cb650c7beb60
Thierry Volpiatto <thievol@posteo.net>
2023-07-12 13:54:58 +0200

    Bump to 1.1

--- wfnames.el
+++ wfnames.el
@@ -6,7 +6,7 @@
 
 ;; Compatibility: GNU Emacs 24.3+"
 ;; Package-Requires: ((emacs "24.3"))
-;; Version: 1.0
+;; Version: 1.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
